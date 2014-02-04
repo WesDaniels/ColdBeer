110612a15fe58f7126c094e4ba03391fb4c61a74
Wesley Daniels <github@wesdaniels.com>
2014-02-03 18:10:00 -0600

    check in

---
+++ CoolBeer.UnitTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolBeer.UnitTest
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void test1()
+        {
+
+        }
+    }
+}
